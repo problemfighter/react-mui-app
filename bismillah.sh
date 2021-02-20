6ecd757dfa0c3d5a23b8115c78dffe8d9b518880
hmtmcse <hmtm.cse@gmail.com>
2021-02-20 20:06:08 +0600

    updated repository link

--- bismillah.sh
+++ bismillah.sh
@@ -24,14 +24,14 @@ echo "Cloning Project react-mui-ui";
 if [ -d "react-mui-ui" ]; then
   rm -rf react-mui-ui
 fi
-git clone https://github.com/hmtmcse/react-mui-ui.git
+git clone https://github.com/problemfighter/react-mui-ui.git
 
 
 echo "Cloning Project tm-react";
 if [ -d "tm-react" ]; then
   rm -rf tm-react
 fi
-git clone https://github.com/hmtmcse/tm-react.git
+git clone https://github.com/problemfighter/tm-react.git
 
 cd ..
 
