27940bbcf31be353f08b3abda1a481bcfd621582
hmtmcse <hmtm.cse@gmail.com>
2021-03-10 10:03:13 +0600

    adding pull all script

--- pull-all.sh
+++ pull-all.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
+echo "Taking Root Project Pull"
 git pull
 
 CURRENT=`pwd`
 SOURCE_DEP="$CURRENT/dependency-source"
-for i in $(ls '$SOURCE_DEP'); do
+echo "$SOURCE_DEP"
+for i in $(ls "$SOURCE_DEP"); do
   echo $i;
 done
