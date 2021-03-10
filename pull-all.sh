33f83e74bd3cd6c1ca6d72c4e4f470420a115e66
hmtmcse <hmtm.cse@gmail.com>
2021-03-10 10:05:24 +0600

    adding pull all script

--- pull-all.sh
+++ pull-all.sh
@@ -7,5 +7,8 @@ CURRENT=`pwd`
 SOURCE_DEP="$CURRENT/dependency-source"
 echo "$SOURCE_DEP"
 for i in $(ls "$SOURCE_DEP"); do
-  echo $i;
+  echo "Taking pull of $i";
+  REPO="$SOURCE_DEP/$i"
+  cd "$REPO"
+  git pull
 done
