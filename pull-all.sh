9277f49da1554c3847bab2d756a491cd0cd29281
hmtmcse <hmtm.cse@gmail.com>
2021-03-10 10:08:36 +0600

    adding pull all script

--- pull-all.sh
+++ pull-all.sh
@@ -5,10 +5,20 @@ git pull
 
 CURRENT=`pwd`
 SOURCE_DEP="$CURRENT/dependency-source"
-echo "$SOURCE_DEP"
 for i in $(ls "$SOURCE_DEP"); do
   echo "Taking pull of $i";
   REPO="$SOURCE_DEP/$i"
   cd "$REPO"
   git pull
 done
+
+APP_DEP="$CURRENT/application/module"
+if [ -d "$APP_DEP" ]; then
+  for i in $(ls "$APP_DEP"); do
+    echo "Taking pull of $i";
+    REPO="$APP_DEP/$i"
+    cd "$REPO"
+    git pull
+  done
+fi
+
