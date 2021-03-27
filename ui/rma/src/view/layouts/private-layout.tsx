e1ff3d5d4ae908934c72b64423e6f453c59a782e
hmtmcse <hmtm.cse@gmail.com>
2021-03-27 09:23:11 +0600

    upgrade files

--- ui/rma/src/view/layouts/private-layout.tsx
+++ ui/rma/src/view/layouts/private-layout.tsx
@@ -3,7 +3,6 @@ import TRReactComponent from "tm-react/src/artifacts/framework/tr-react-componen
 import TRLayoutRenderer from "tm-react/src/artifacts/component/tr-layout-rander";
 import {withStyles} from "react-mui-ui/ui/ui-component";
 import {privateLayoutJSS} from "react-material-app/src/assets/style-jss";
-import {AppConstant} from "../../system/app-constant";
 import AppNavigation from "../../system/app-navigation";
 import NavigationBar from "../nav/navigation-bar";
 import TRBrowserStorageManager from "tm-react/src/artifacts/manager/tr-browser-storage-manager";
@@ -14,13 +13,16 @@ class PrivateLayout extends TRReactComponent<any, any> {
 
     render() {
         const {component, route, appConfig, classes} = this.props;
+
         let renderView = (
+            // @ts-ignore
             <NavigationBar
                 route={route}
                 appConfig={appConfig}
                 itemList={AppNavigation.getNavigation(route)}
                 appTitle={AppMessage.appName}
-                bodyContent={<TRLayoutRenderer route={route} appConfig={appConfig} component={component}/>}/>
+                bodyContent={<TRLayoutRenderer route={route} appConfig={appConfig} component={component}/>}
+            />
             );
 
         if (!Boolean(TRBrowserStorageManager.getByKey("isAuthorized"))) {
