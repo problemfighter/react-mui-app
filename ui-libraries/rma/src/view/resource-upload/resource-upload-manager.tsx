3861d93064685834e3f16571739697cc5a69f8a7
hmtmcse <hmtm.cse@gmail.com>
2021-04-23 16:51:43 +0600

    adding modules

--- ui-libraries/rma/src/view/resource-upload/resource-upload-manager.tsx
+++ ui-libraries/rma/src/view/resource-upload/resource-upload-manager.tsx
@@ -42,15 +42,15 @@ export default class ResourceUploadManager extends TRComponent<Props, State> {
 
     }
 
-    getPreviewView(file: File, index: any) {
+    loadThumb(file: File, index: any) {
         return (
             <Grid item xs={3} key={index}>
                 <Card>
                     <CardActionArea>
                         <CardMedia
                             component="img"
-                            height="120"
-                            width="120"
+                            height="200"
+                            width="200"
                             image={URL.createObjectURL(file)}
                         />
                     </CardActionArea>
@@ -70,26 +70,59 @@ export default class ResourceUploadManager extends TRComponent<Props, State> {
         )
     }
 
+    deleteUploadedPreview(event: any, index: any) {
+        let data = this.getValueFromFormData(this.getInputName())
+        if (data && data instanceof Array && data.length !== 0 && data[0] instanceof File) {
+            data.splice(index, 1);
+            this.setValueToFormData(this.getInputName(), data)
+        }
+    }
+
+    getUploadPreviewView(file: File, index: any) {
+        return (
+            <Grid item xs={3} key={index}>
+                <Card>
+                    <CardActionArea>
+                        <CardMedia
+                            component="img"
+                            height="200"
+                            width="200"
+                            image={URL.createObjectURL(file)}
+                        />
+                    </CardActionArea>
+                    <Box textAlign="center">
+                        <IconButton color="secondary" component="span" title="Delete the resources" onClick={(event: any) => {this.deleteUploadedPreview(event, index)}}>
+                            <DeleteIcon/>
+                        </IconButton>
+                    </Box>
+                </Card>
+            </Grid>
+        )
+    }
+
     loadPreview(files: any) {
         let _this = this;
-        console.log(files)
         if (files) {
             let view: any = []
             files.forEach((file: any, index: any) => {
-                view.push(_this.getPreviewView(file, index))
+                view.push(_this.getUploadPreviewView(file, index))
             })
             return view
         }
         return ""
     }
 
+    getInputName() {
+        if (this.props.inputName) {
+            return this.props.inputName
+        }
+        return ""
+    }
 
     renderUI() {
         let _this = this;
         let {title, uploadButtonLabel, inputName} = this.props
-        if (!inputName) {
-            inputName = ""
-        }
+        inputName = _this.getInputName()
         return (
             <React.Fragment>
                 <Grid container spacing={1} justify="space-between" alignItems="center">
