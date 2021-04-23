135d2b18360136947158a0f27d6b429850d2d484
hmtmcse <hmtm.cse@gmail.com>
2021-04-23 15:50:49 +0600

    adding modules

--- ui-libraries/rma/src/view/resource-upload/resource-upload-manager.tsx
+++ ui-libraries/rma/src/view/resource-upload/resource-upload-manager.tsx
@@ -6,7 +6,7 @@ import {
     Button, Card, CardActionArea,
     CardActions,
     CardContent,
-    CardHeader, CardMedia, DeleteIcon,
+    CardHeader, CardMedia, CloudUploadIcon, DeleteIcon,
     Divider, EditIcon,
     Grid, IconButton, PhotoCameraIcon, TextField
 } from "react-mui-ui/ui/ui-component";
@@ -15,11 +15,22 @@ import React from "react";
 
 interface Props extends TRProps {
     route: any;
+    title?: string
+    uploadButtonLabel?: string
+    inputName?: string
+    gridSize?: bigint
 }
 
 class State extends TRComponentState{}
 
 export default class ResourceUploadManager extends TRComponent<Props, State> {
+
+    static defaultProps = {
+        title: "Resources",
+        uploadButtonLabel: "Upload",
+        gridSize: 3,
+        inputName: "file",
+    };
 
     state: State = new State();
 
@@ -31,52 +42,72 @@ export default class ResourceUploadManager extends TRComponent<Props, State> {
 
     }
 
+    getPreviewView(file: File, index: any) {
+        return (
+            <Grid item xs={3} key={index}>
+                <Card>
+                    <CardActionArea>
+                        <CardMedia
+                            component="img"
+                            height="120"
+                            width="120"
+                            image={URL.createObjectURL(file)}
+                        />
+                    </CardActionArea>
+                    <Box textAlign="center">
+                        <IconButton color="primary" component="span" title="Update meta information">
+                            <EditIcon/>
+                        </IconButton>
+                        <IconButton color="secondary" component="span" title="Delete the resources">
+                            <DeleteIcon/>
+                        </IconButton>
+                        <IconButton color="primary" component="span" title="Replace the resources">
+                            <CloudUploadIcon/>
+                        </IconButton>
+                    </Box>
+                </Card>
+            </Grid>
+        )
+    }
+
+    loadPreview(files: any) {
+        let _this = this;
+        console.log(files)
+        if (files) {
+            let view: any = []
+            files.forEach((file: any, index: any) => {
+                view.push(_this.getPreviewView(file, index))
+            })
+            return view
+        }
+        return ""
+    }
+
 
     renderUI() {
         let _this = this;
+        let {title, uploadButtonLabel, inputName} = this.props
+        if (!inputName) {
+            inputName = ""
+        }
         return (
             <React.Fragment>
                 <Grid container spacing={1} justify="space-between" alignItems="center">
                     <Grid item xs={6}>
-                        <CardHeader title="Upload"/>
+                        <CardHeader title={title}/>
                     </Grid>
                     <Grid item xs={1} >
                         <Button color="primary" variant="contained" component="label">
-                            Upload
-                            <input type="file" hidden {...this.handleInputDataChange("file")} />
+                            {uploadButtonLabel}
+                            <input type={inputName} multiple hidden {...this.handleInputDataChange(inputName)} />
                         </Button>
                     </Grid>
                 </Grid>
                 <Divider/>
                 <CardContent>
                     <Grid  container spacing={4}>
-                        <Grid item xs={3}>
-                            <Card>
-                                <CardActionArea>
-                                    <CardMedia
-                                        component="img"
-                                        height="120"
-                                        width="120"
-                                        image="https://imgd.aeplcdn.com/476x268/n/cw/ec/38904/mt-15-front-view.jpeg"
-                                    />
-                                </CardActionArea>
-                                <Box textAlign="center">
-                                    <IconButton color="primary" component="span">
-                                        <EditIcon/>
-                                    </IconButton>
-                                    <IconButton color="secondary" component="span">
-                                        <DeleteIcon/>
-                                    </IconButton>
-                                    <IconButton color="primary" component="span">
-                                        <PhotoCameraIcon/>
-                                    </IconButton>
-                                </Box>
-                            </Card>
-                        </Grid>
+                        { _this.loadPreview(this.state.formData[inputName])}
                     </Grid>
-
-
-
                 </CardContent>
                 <CardActions>
                     <Grid container spacing={1} justify="flex-end">
